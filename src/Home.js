8867a00c4435330c2c67b978b97f1f22c463ff16
Shaileshsainee123 <sk19810848@gmail.com>
2022-06-28 12:49:08 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import Search from './Search';
+import Movie from './Movie'
+
+
+const Home = () => {
+ // const name= useContext(MyContext);
+
+  return (
+    <>
+    <Search />
+   <Movie />
+    </>
+  )
+}
+
+export default Home
---
+++ src/Movie.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useGlobalContext } from "./context";
+
+const imgUrl = "https://via.placeholder.com/200/200";
+
+const Movie = () => {
+  const { movie,isLoading } = useGlobalContext();
+  if(isLoading){
+    return <div className="loading">Loading....</div>;
+  }
+  return (
+    <section className="movie-page">
+      <div className="container grid grid-4-col">
+        { movie ? movie.map((curmovie) => {
+          const { imdbID, Title, Poster } = curmovie;
+          const movieName=Title.substring(0,15);
+          return(
+          <NavLink to={`movie/${imdbID}`} key={imdbID}>
+            <div className="card">
+              <div className="card-info">
+                <h2>{movieName>=13 ? `${movieName}..` : movieName}</h2>
+                <img src={Poster === "N/A" ? imgUrl : Poster} alt={imdbID} />
+              </div>
+            </div>
+          </NavLink>
+          );
+        }) : ""}
+      </div>
+    </section>
+  );
+};
+
+export default Movie;
